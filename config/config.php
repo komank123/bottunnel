7db274b764e8201d986e15d2ddb28b33d43766bc
komank123 <komank123@users.noreply.github.com>
2022-09-15 13:09:02 +0800

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+$datasa["ipaddress"] 	   = "27.112.79.155";                     //Ubah ip router anda            wajib
+$datasa["user"] 				= "admin";                 //ubah username mirktoik       wajib
+$datasa["password"] 	    	= "Mahadev9";                //paswword router      wajib
+$datasa["token"] 		    	= "1678277677:AAGzrHcVrne9FcTITDuX9iRnFQC2OUBbx08";                //token Bot    wajib
+$datasa["usernamebot"]     = "@Bobonet_bot";           //usernamebot wajib
+$datasa["id"] 			    	= "";   //paswwordbot comingsoon
